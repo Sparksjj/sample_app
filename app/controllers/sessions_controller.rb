3b12eccbdfb9ccbea3c912d9b1cc6af233921493
Yauhen <Arucard2009@yandex.ru>
2016-01-22 21:54:11 +0300

    add token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user=User.find_by(email: params[:session][:email].downspase)
-		if user && user.authenticate(params[:session][:pasword])
+		user=User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
 			sign_in user
 			redirect_to user
 		else
