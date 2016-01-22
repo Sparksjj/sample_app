02401096317b3185f8fd9ce1641ef70711d4e6e8
Yauhen <Arucard2009@yandex.ru>
2016-01-22 16:40:54 +0300

    afsaf

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user=User.find_by(email: params[:sesseon][:email].downspase)
+		user=User.find_by(email: params[:session][:email].downspase)
 		#if user && user.authenticate(params[:sesseon][:pasword])
 		#	sign_in user
 		#	redirect_to user
