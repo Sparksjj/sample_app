66ad037cf0dedb845dd44a398d4ee01629d49017
Yauhen <Arucard2009@yandex.ru>
2016-01-22 15:33:59 +0300

    Sign_in made

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		
+		user=User.find_by(email: params[:sesseon][:email].downspase)
+		if user && user.authenticate(params[:sesseon][:pasword])
+			sign_in user
+			redirect_to user
+		else
+			flash.now[:error]="Invalid email/password combination."
+			render "new"
+
+		end
 	end
 
 	def destroy
