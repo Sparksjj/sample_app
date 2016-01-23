060a332b8d335cc894c969930cee305dbea40eb9
Yauhen <Arucard2009@yandex.ru>
2016-01-23 23:01:48 +0300

    Cookies bilding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 	def sign_in(user)
-		remember_token=User.new_remwmber_token
+		remember_token=User.new_remember_token
 		kookies.permanent[:remember_token]=remember_token
 		user.update_attribute(:remember_token, User.encrypt(remember_token))
 		self.current_user=user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	before_save { self.email=email.downcase! }
-	befor_create :crete_remember_token
+	before_create :crete_remember_token
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true,
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 	end
 
 	def User.encrypt(token)
-		Digest::SHA1.hexdigest(token)
+		Digest::SHA1.hexdigest(token.to_s)
 	end
 
 	private
