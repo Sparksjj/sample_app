9a52e8477601003c949adb5df0070b6b6c08bb22
Yauhen <Arucard2009@yandex.ru>
2016-01-24 15:08:55 +0300

    Fixed nil email problem and add user header

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	before_save { self.email=email.downcase! }
+	before_save { self.email.downcase! }
 	before_create :crete_remember_token
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   #get 'static_pages/help'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/sigout',  to: 'sessions#destroy',     via: 'delete'
+  match '/signout',  to: 'sessions#destroy',     via: 'delete'
   match '/help', to: 'static_pages#help',       via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
